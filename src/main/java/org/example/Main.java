28f739ae9781aaa2b32ff115b00e40a7ef3fec3c
SergioBalca <SergioBalca@github.com>
2023-01-27 17:34:39 -0500

    Agregar en paquete example3 la clase SeriesList

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 import org.example.list.arrayListExamples.example1.ListaDeCompras;
 import org.example.list.arrayListExamples.example2.AccesoEquipos;
 import org.example.list.arrayListExamples.example2.ManejoLista;
+import org.example.list.arrayListExamples.example3.SeriesList;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,5 +15,9 @@ public class Main {
         System.out.println("Ejemplo2");
         AccesoEquipos accesoEquipos = new AccesoEquipos();
         accesoEquipos.manipularEquipos();
+        System.out.println("----------------------------------------------------------");
+        System.out.println("Ejemplo 3");
+        SeriesList seriesList = new SeriesList();
+        System.out.println("La lista de seires contiene a the Wire? " + seriesList.getSeries().contains("The Wire"));
     }
 }
