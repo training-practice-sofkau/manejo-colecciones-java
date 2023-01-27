693122ee856fd4f64b5da6cea8f75c6ea6e40313
Santiago Posada <santiago90dell@gmail.com>
2023-01-27 12:28:03 -0500

    base project ready

---
+++ src/main/java/org/example/list/ArrayListPractice.java
@@ -0,0 +1,10 @@
+package org.example.list;
+
+public class ArrayListPractice {
+    /**
+     * Crear ejemplos en donde se tenga que implementar estos tipos de dato,
+     * minimo 3 escenarios por cada uno, si genera un escenario integrando el conocimiento
+     * previo y que involucre varios tipos de dato, vale por uno de cada uno de los tipos
+     * de dato involucrados.
+     * */
+}
