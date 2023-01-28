55c753c8e5a51f1154eb4c60ae425d33c11b5227
Eros Jose Adarraga Jimenez <dev.erosadarraga@gmail.com>
2023-01-28 10:21:49 -0500

    Cambios finales

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,33 @@
 package org.example;
 
+import org.example.list.ArrayListPractice;
+import org.example.list.LinkedListPractice;
+import org.example.map.HashMapPractice;
+import org.example.set.HashSetPractice;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("***********   ArrayListBuilder  ***********");
+        System.out.println("");
+        ArrayListPractice arrayListPractice = new ArrayListPractice();
+        arrayListPractice.run();
+
+        System.out.println("");
+        System.out.println("***********   LinkedListSingleton  ***********");
+        System.out.println("");
+        LinkedListPractice linkedListPractice = new LinkedListPractice();
+        linkedListPractice.run();
+
+        System.out.println("");
+        System.out.println("***********   HashMapPractice  ***********");
+        System.out.println("");
+        HashMapPractice hashMapPractice = new HashMapPractice();
+        hashMapPractice.run();
+
+        System.out.println("");
+        System.out.println("***********   HashSetPractice  ***********");
+        System.out.println("");
+        HashSetPractice hashSetPractice = new HashSetPractice();
+        hashSetPractice.run();
     }
 }
--- src/main/java/org/example/list/ArrayListPractice.java
+++ src/main/java/org/example/list/ArrayListPractice.java
@@ -1,6 +1,19 @@
 package org.example.list;
+import java.util.ArrayList;
+
 
 public class ArrayListPractice {
+
+    public void run (){
+        ArrayList<Integer> list = new ArrayListBuilder<Integer>()
+                .add(10)
+                .add(60)
+                .add(89)
+                .build();
+        System.out.println(list.toString());
+    }
+
+
     /**
      * Crear ejemplos en donde se tenga que implementar estos tipos de dato,
      * minimo 3 escenarios por cada uno, si genera un escenario integrando el conocimiento
@@ -8,3 +21,27 @@ public class ArrayListPractice {
      * de dato involucrados.
      * */
 }
+ class ArrayListBuilder<E> {
+    private ArrayList<E> list;
+
+    public ArrayListBuilder() {
+        list = new ArrayList<E>();
+    }
+
+    public ArrayListBuilder<E> add(E element) {
+        list.add(element);
+        System.out.println(element);
+        return this;
+    }
+
+    public ArrayList<E> build() {
+        return list;
+    }
+
+     @Override
+     public String toString() {
+         return "ArrayListBuilder{" +
+                 "list=" + list +
+                 '}';
+     }
+ }
