1a7859cd84516c23e14c77a2f78d1fb3c0cfcdb7
LAURA <vanessa.alba1427@gmail.com>
2023-01-27 22:59:33 -0500

    Ejemplo 1 de ArrayList

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import org.example.list.ArrayListPractice;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        ArrayListPractice arrayListPractice = new ArrayListPractice();
+        arrayListPractice.ejemplo1();
+
     }
 }
--- src/main/java/org/example/list/ArrayListPractice.java
+++ src/main/java/org/example/list/ArrayListPractice.java
@@ -1,5 +1,7 @@
 package org.example.list;
 
+import java.util.ArrayList;
+
 public class ArrayListPractice {
     /**
      * Crear ejemplos en donde se tenga que implementar estos tipos de dato,
@@ -7,4 +9,19 @@ public class ArrayListPractice {
      * previo y que involucre varios tipos de dato, vale por uno de cada uno de los tipos
      * de dato involucrados.
      * */
+    public void ejemplo1(){
+        ArrayList<Auto> misAutos = new ArrayList<>();
+        misAutos.add(new Auto("Renault","Gris"));
+        misAutos.add(new Auto("Mazda","Negro"));
+        misAutos.add(new Auto("Toyota","Blanco"));
+
+        System.out.println("El auto 2° es: " +misAutos.get(1));
+
+        System.out.println("Lista de autos");
+        for (int i =0; i < misAutos.size(); i++){
+            System.out.println("El auto en la posicion "+ i+ " es "+ misAutos.get(i));
+        }
+
+    }
+
 }
