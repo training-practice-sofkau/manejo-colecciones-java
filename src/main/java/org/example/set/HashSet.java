43be64d62ee30d575db058e973881b97210a22de
efsolora <efsolora@gmail.com>
2023-01-28 17:20:23 -0500

    ejemplos hashSet

--- src/main/java/org/example/set/HashSet.java
+++ src/main/java/org/example/set/HashSet.java
@@ -1,4 +1,20 @@
 package org.example.set;
 
 public class HashSet {
+
+    public void ejecutar(){
+        CasaVotaciones casaVotaciones = new CasaVotaciones("Nuestra Señora", "cr40#46-40");
+        Votante votante1 = new Votante(2222222, "Efrain",1, casaVotaciones);
+        Votante votante2 = new Votante(2222222, "Lorena",2, casaVotaciones);
+        Votante votante3 = new Votante(2222222, "Cristian",3, casaVotaciones);
+        Jurado jurado1 = new Jurado(2222222, "juan",1, casaVotaciones);
+        casaVotaciones.agregarVotantes(votante1);
+        casaVotaciones.agregarVotantes(votante2);
+        casaVotaciones.agregarVotantes(votante3);
+        casaVotaciones.agregarVotantes(jurado1);
+        casaVotaciones.mostrarVotacion();
+        casaVotaciones.numeroJurados();
+        casaVotaciones.numeroVotantes();
+    }
+
 }
