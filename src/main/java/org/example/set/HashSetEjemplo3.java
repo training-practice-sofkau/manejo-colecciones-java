ba963f021d1cbae8d83f9468aaef4607a753a0b7
Bryan Polo <david2315@hotmail.com>
2023-01-29 17:58:28 -0500

    Ejercicios terminados

--- src/main/java/org/example/set/HashSetEjemplo3.java
+++ src/main/java/org/example/set/HashSetEjemplo3.java
@@ -1,4 +1,22 @@
 package org.example.set;
 
+import java.util.HashSet;
+
 public class HashSetEjemplo3 {
+    public static void main(String[] args) {
+        HashSet<String> set = new HashSet<>();
+
+        set.add("dog");
+        set.add("cat");
+        set.add("mouse");
+        set.add("tiger");
+        set.add("lion");
+
+        System.out.println("HashSet elements: " + set);
+
+        set.remove("mouse");
+
+        System.out.println("After removing 'mouse': " + set);
+    }
 }
+
