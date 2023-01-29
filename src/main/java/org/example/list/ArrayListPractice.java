f51687624c9b314cf29af4aebe272a8fc8e469c0
katherine Bonilla <felicidadkg@gmail.com>
2023-01-29 18:39:52 -0500

    listas

--- src/main/java/org/example/list/ArrayListPractice.java
+++ src/main/java/org/example/list/ArrayListPractice.java
@@ -1,5 +1,8 @@
 package org.example.list;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ArrayListPractice {
     /**
      * Crear ejemplos en donde se tenga que implementar estos tipos de dato,
@@ -7,4 +10,91 @@ public class ArrayListPractice {
      * previo y que involucre varios tipos de dato, vale por uno de cada uno de los tipos
      * de dato involucrados.
      * */
+    public static void main(String[] args) {
+
+        //ArrayList Empleados
+
+        List<Empleado> listaDeEmpleados = new ArrayList<>();
+
+        listaDeEmpleados.add(new Empleado("Carlos", "Leon", 25822, "leon123@gmail.com", 180000.00  ));
+        listaDeEmpleados.add(new Empleado("Marta", "Perez", 5222, "perez123@gmail.com", 410000.00 ));
+        listaDeEmpleados.add(new Empleado("Lucy", "Vivas",  369, "Vivas123@gmail.com", 280000.00 ));
+        listaDeEmpleados.add(new Empleado("Juan", "Telon", 59877, "telon123@gmail.com", 190090.20 ));
+        listaDeEmpleados.add(new Empleado("Camila", "Bahia", 2357, "baiha123@gmail.com", 250000.00 ));
+        listaDeEmpleados.add(new Empleado("Jenny", "Lopez", 3657, "lopez123@gmail.com", 180000.00 ));
+
+
+        //recorrer la lista
+
+        for (Empleado empleado : listaDeEmpleados) {
+            System.out.println(" ");
+            System.out.println("Nombre del empleado: " + empleado.getName());
+            System.out.println("Apellido del empleado: " + empleado.getLastName());
+            System.out.println("Número de empleado: " + empleado.getNumber());
+            System.out.println(" Correo del empleado: " + empleado.getEmail());
+            System.out.println("Salario del empleado: " + empleado.getSalary());
+
+        }
+
+
+    }
+
+
+
 }
+ class Empleado{
+    private String name;
+    private String lastName;
+
+    private int number;
+    private String email;
+    private double salary;
+
+     public Empleado(String name, String lastName, int number, String email, Double salary) {
+         this.name = name;
+         this.lastName = lastName;
+         this.number = number;
+         this.email = email;
+         this.salary = salary;
+     }
+
+     public String getName() {
+         return name;
+     }
+
+     public void setName(String name) {
+         this.name = name;
+     }
+
+     public String getLastName() {
+         return lastName;
+     }
+
+     public void setLastName(String lastName) {
+         this.lastName = lastName;
+     }
+
+     public String getEmail() {
+         return email;
+     }
+
+     public void setEmail(String email) {
+         this.email = email;
+     }
+
+     public int getNumber() {
+         return number;
+     }
+
+     public void setNumber(int number) {
+         this.number = number;
+     }
+
+     public Double getSalary() {
+         return salary;
+     }
+
+     public void setSalary(Double salary) {
+         this.salary = salary;
+     }
+ }
--- src/main/java/org/example/list/LinkedListPractice.java
+++ src/main/java/org/example/list/LinkedListPractice.java
@@ -1,4 +1,65 @@
 package org.example.list;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class LinkedListPractice {
+
+    public static void main(String[] args) {
+
+        List<User> usuarios = new LinkedList<>();
+
+        usuarios.add(new User ("Jhon Oqua", "Jhon@gmail.com", "58game"));
+        usuarios.add(new User ("Rita Lope", "Lope@gmail.com", "kl_458"));
+        usuarios.add(new User ("Camila Key", "Kay@gmail.com", "llave25"));
+        //agregar al final
+
+        System.out.println("Final de la lista: ");
+        usuarios.add(new User ("Pedro Rueda", "rueda@gmail.com", "bogota5"));
+     //agregar al principio
+        System.out.println("Principio de la lista: ");
+        usuarios.add(0, new User("Ana Lima", "lima@gmail.com", "4587lima"));
+
+
+       for(User usuario : usuarios){
+           System.out.println("Usuario: " + usuario.getName() );
+       }
+
+    }
+}
+
+class User{
+    private String name;
+    private String email;
+    private String password;
+
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
