77b0bdb1ea89711d1c6b78344bbb3182decd420f
MiguelMendoza22 <jesusmiguelmm@ufps.edu.co>
2023-01-29 11:03:30 -0500

    Se crean las clases Contacto, Carro y Persona, se crean metodos en la clase ArrayListPractice con el argoritmo de ejecucion para cada una de las clases y se instancian en la clase main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,15 @@
 package org.example;
 
+import org.example.list.ArrayListPractice;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        ArrayListPractice arrayListPractice = new ArrayListPractice();
+        //arrayListPractice.ejemploUnoArrayList();
+        //arrayListPractice.ejemploDosArrayList();
+        arrayListPractice.ejemploTresArrayList();
+
     }
+
 }
