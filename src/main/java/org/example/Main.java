2c22ed2b12fbd95708ec6b5ab506eac8eaf540d3
POO <juandieguin2016@gmail.com>
2023-01-29 18:42:25 -0500

    Ejemplos LinkedList

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,18 +2,30 @@ package org.example;
 
 import org.example.list.ArrayListPractice;
 import org.example.list.LinkedListPractice;
+import org.example.map.HashMapPractice;
+
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
+
+        System.out.println("oooooooooooooooooooooooooooooooooooooooooooooooooooooo");
         System.out.println("Array List");
 
         ArrayListPractice prueba = new ArrayListPractice();
         prueba.ejecutarArray();
 
+        System.out.println("oooooooooooooooooooooooooooooooooooooooooooooooooooooo");
         System.out.println("LinkedList");
 
         LinkedListPractice prueba1 = new LinkedListPractice();
         prueba1.ejecutarLinked();
+
+        System.out.println("oooooooooooooooooooooooooooooooooooooooooooooooooooooo");
+        System.out.println("HashMap");
+
+        HashMapPractice pruebas2 = new HashMapPractice();
+        pruebas2.ejecutar();
 
     }
 }
--- src/main/java/org/example/map/HashMapPractice.java
+++ src/main/java/org/example/map/HashMapPractice.java
@@ -1,4 +1,47 @@
 package org.example.map;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 public class HashMapPractice {
+    public void ejecutar(){
+
+        System.out.println("------EJEMPLO 1--------");
+
+        HashMap<Integer, String> hash1 = new HashMap<>();
+        hash1.put(1000,"Carlos");
+        hash1.put(3000,"Paula");
+        hash1.put(8000,"Monica");
+
+        System.out.println(hash1);
+
+        if (hash1.containsKey(3000)) {
+            System.out.println("Contiene el valor 3000: " + hash1.get(3000));
+        } else{
+                System.out.println("No contiene 3000");
+            }
+        if (hash1.containsKey(100)) {
+            System.out.println("Contiene el valor 3000: " + hash1.get(100));
+        } else{
+            System.out.println("No contiene 3000");
+        }
+
+
+        System.out.println("---------EJEMPLO 2-----------");
+
+        Map<Integer, Estudiante> listaEstudiantes = new HashMap<>();
+
+        Estudiante estudiante1 = new Estudiante("Carlos", 24, new Date());
+        Estudiante estudiante2 = new Estudiante("Mauricio", 27, new Date());
+        Estudiante estudiante3 = new Estudiante("Maicol", 30, new Date());
+
+        listaEstudiantes.put(1,estudiante1);
+        listaEstudiantes.put(2,estudiante2);
+        listaEstudiantes.put(3,estudiante3);
+
+        for (Map.Entry<Integer, Estudiante> est: listaEstudiantes.entrySet()){
+            System.out.println("Clave del estudiante: " + est.getKey());
+        }
+    }
 }
