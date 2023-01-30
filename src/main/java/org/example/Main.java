86e9e9069f8f84a94a314c92f95d2cef60a3de57
juan david cardona velasquez <j.cardona1@utp.edu.co>
2023-01-29 23:16:12 -0500

    Ejemplos de Hash Map Terminados

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,9 @@ package org.example;
 
 import org.example.list.ArrayListPractice;
 import org.example.list.LinkedListPracticeBuilder;
+import org.example.map.HashMapPractice;
 
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
@@ -21,23 +23,31 @@ public class Main {
             switch (o){
                 case 1:
                     //Array List///////////
+                    System.out.println("*****Array List*****");
                     ArrayListPractice arrayList = new ArrayListPractice();
                     arrayList.arrayListObjects();
                     arrayList.simpleArrayList();
                     arrayList.arrayFrutas();
                     break;
                 case 2:
-                    System.out.println("Linked List");
+                    System.out.println("\n*****Linked List*****");
                     LinkedListPracticeBuilder linked = new LinkedListPracticeBuilder();
                     linked.ejecutarLinkedList();
                     break;
 
                 case 3:
+                    System.out.println("\n*****Hash Map*****");
+                    HashMapPractice hash = new HashMapPractice();
+                    hash.hashMap1();
+                    hash.hashMap2();
+                    hash.hashMap3();
                     break;
                 case 4:
+                    System.out.println("\n*****Hash Set*****");
+
                     break;
                 default:
-                    System.out.println("Opcion Incorrecta");
+                    System.out.println("\n*****Opcion Incorrecta*****");
                     bandera = false;
                     break;
             }
