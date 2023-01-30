2e8ed2d6c1b22337f5a43b778a7bc39fbd8e6365
nunezjose770 <nunezjose770@gmail.com>
2023-01-29 21:57:24 -0500

    Fisrt commit, Array list

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,20 @@
 package org.example;
 
+import org.example.list.ArrayListPractice;
+import org.example.list.ArrayListPractice2;
+import org.example.list.ArrayListPractice3;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("-----ArrayList-----");
+        System.out.println("Ejemplo 1");
+        ArrayListPractice arrayListPractice = new ArrayListPractice();
+        arrayListPractice.ejecutar();
+        System.out.println("Ejemplo 2");
+        ArrayListPractice2 arrayListPractice2 = new ArrayListPractice2();
+        arrayListPractice2.ejectuar();
+        System.out.println("Ejemplo 3");
+        ArrayListPractice3 arrayListPractice3 = new ArrayListPractice3();
+        arrayListPractice3.ejecutar();
     }
 }
--- src/main/java/org/example/list/ArrayListPractice.java
+++ src/main/java/org/example/list/ArrayListPractice.java
@@ -1,5 +1,7 @@
 package org.example.list;
 
+import java.util.ArrayList;
+
 public class ArrayListPractice {
     /**
      * Crear ejemplos en donde se tenga que implementar estos tipos de dato,
@@ -7,4 +9,33 @@ public class ArrayListPractice {
      * previo y que involucre varios tipos de dato, vale por uno de cada uno de los tipos
      * de dato involucrados.
      * */
+
+    public void ejecutar(){
+        ArrayList<Person> people = new ArrayList<>();
+        people.add(new Person("Andres", 30));
+        people.add(new Person("David", 35));
+        people.add(new Person("Camila", 25));
+
+        for (Person person : people) {
+            System.out.println(person.getName() + " tiene " + person.getAge() + " años");
+        }
+    }
 }
+
+    class Person {
+        private String name;
+        private int age;
+
+        public Person(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+    }
